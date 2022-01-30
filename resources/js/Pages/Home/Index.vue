9f00cfcbf12f5a1afbf159a43d089db7c63e7587
miguelbonifaz <miguelbonifaz126@gmail.com>
2022-01-30 12:04:43 -0500

    modal was removed

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -157,17 +157,7 @@
         </StackedList>
     </div>
 
-    <Modal>
-        <template v-slot:default> content@! </template>
-        <template v-slot:footer>
-            <button
-                type="button"
-                class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-                Realizar Pedido
-            </button>
-        </template>
-    </Modal>
+
 </template>
 
 <style scoped></style>
